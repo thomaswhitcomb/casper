19b0d138549d1aa6fa1674a7742a90abe4910fc2
Tom Whitcomb <Tom_Whitcomb@intuit.com>
2014-10-01 08:09:47 -0700

    did some refactoring in the db module

--- src/casper/db.clj
+++ src/casper/db.clj
@@ -1,50 +1,53 @@
 (ns casper.db
-  (:require [taoensso.faraday :as far] )
+  (:require [taoensso.faraday :as dynamodb] )
   (:gen-class)
 )
 
+(def table :casper)
+(def local-db "none")
+
+(defn get-property [field] (. System getProperty field))
 (defn now-seconds [] (quot (System/currentTimeMillis) 1000))
+(defn get-aws-key [k] (if-let [id (get-property k) ] id local-db))
 
 (def client-opts 
   (let [
-        access-key (if-let [k (. System getProperty "AWS_ACCESS_KEY_ID") ] k "none")
-        secret-key (if-let [k (. System getProperty "AWS_SECRET_KEY") ] k "none")
+        access-key (get-aws-key "AWS_ACCESS_KEY_ID")
+        secret-key (get-aws-key "AWS_SECRET_KEY") 
         opts {:access-key access-key, :secret-key secret-key}
        ]
-    (if (= "none" access-key)
-      (assoc opts :endpoint"http://localhost:8000")
-      opts
-    )  
+      (if (= local-db access-key)
+        (assoc opts :endpoint"http://localhost:8000")
+        opts
+      )  
   )
 )  
 
 
-(def table :casper)
-
 (defn drop-db []
-  (let [tables (far/list-tables client-opts)]
+  (let [tables (dynamodb/list-tables client-opts)]
     (if (= tables [table])
-      (far/delete-table client-opts table)
+      (dynamodb/delete-table client-opts table)
     )  
   )  
 )
 
 (defn create-db []
-  (let [tables (far/list-tables client-opts)]
+  (let [tables (dynamodb/list-tables client-opts)]
     (if (= 0 (count tables)) 
-      (far/create-table client-opts table [:key :s] {:throughput {:read 1 :write 1} :block? true})
+      (dynamodb/create-table client-opts table [:key :s] {:throughput {:read 1 :write 1} :block? true})
     )  
   )  
 )
 
 (defn insert-secret [key secret ttl] 
-  (far/put-item client-opts table {:key key :secret secret :ttl ttl :created_at (now-seconds) })
+  (dynamodb/put-item client-opts table {:key key :secret secret :ttl ttl :created_at (now-seconds) })
 ) 
 (defn select-secret [key ] 
-  (far/get-item client-opts table {:key key})
+  (dynamodb/get-item client-opts table {:key key})
 ) 
 (defn delete-secret [key ] 
-  (far/delete-item client-opts table {:key key})
+  (dynamodb/delete-item client-opts table {:key key})
 )
 (println client-opts)
 (create-db)
--- test-bulk.sh
+++ test-bulk.sh
@@ -1,18 +1,23 @@
 #!/bin/sh 
 HOST="casper-dev.elasticbeanstalk.com"
 #HOST="localhost:50000"
-for VARIABLE1 in {1..300}
-do
-  for VARIABLE2 in {1..10}
-  do
+
+request (){
 	  # Good create no TTL (use default)
-	  URL=`curl --silent --data-urlencode "json={\"secret\": \"$VARIABLE2\",\"ttl\":100}"  http://${HOST}/create`
+	  URL=`curl --silent --data-urlencode "json={\"secret\": \"$1\",\"ttl\":100}"  http://${HOST}/create`
 	  SECRET=`curl --silent -w "%{http_code}" $URL`
-	  if [ "$SECRET" == "${VARIABLE2}200" ];then
+	  if [ "$SECRET" == "${1}200" ];then
 	          echo "Test - OK"
 	  else
 	          echo "Test - Failed"
 	  fi
+}	  
+
+for VARIABLE1 in {1..300}
+do
+  for VARIABLE2 in {1..5}
+  do
+	  request  $VARIABLE2 &
   done
   sleep 1  
 done
